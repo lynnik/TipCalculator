4ee617477b5d5f404d60d1e1b740e81cb9bf2168
Lynnik Vlad <lynnik.v@gmail.com>
2017-10-06 14:41:46 +0300

    Added MainActivity class variables.

--- app/src/main/java/com/lynnik/tipcalculator/MainActivity.java
+++ app/src/main/java/com/lynnik/tipcalculator/MainActivity.java
@@ -1,9 +1,24 @@
-package com.lynnik.tipcalculator;
+package com.lynnik.tipcalculator; 
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+
+public class MainActivity extends Activity {
+
+  private static final NumberFormat currencyFormat =
+      NumberFormat.getCurrencyInstance();
+  private static final NumberFormat percentFormat =
+      NumberFormat.getPercentInstance();
+
+  private double billAmount = 0.0;
+  private double percent = 0.15;
+  private TextView amountTextView;
+  private TextView percentTextView;
+  private TextView tipTextView;
+  private TextView totalTextView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
